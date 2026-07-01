e0896523203009d0daff56c5f6438fd7585913ac
digitaldevine <mishraprakhar2580@gmail.com>
2026-07-01 20:08:39 +0530

    Update backend API URL

--- src/Component/Signin.js
+++ src/Component/Signin.js
@@ -23,7 +23,7 @@ function SignIn() {
   const handleLogin = async () => {
     try {
       const response = await fetch(
-        "https://backend-7aq8.onrender.com/api/auth/signin",
+        "https://backend-7aq8.onrender.com/api/auth/signup",
         {
           method: "POST",
           headers: {
@@ -62,7 +62,7 @@ function SignIn() {
 
     try {
       const response = await fetch(
-        "https://backend-7aq8.onrender.com/api/auth/signup",
+        "https://backend-7aq8.onrender.com/api/auth/signin",
         {
           method: "POST",
           headers: {
